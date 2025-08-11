afccbfac040e1bcb4955b9b98cf0e958dd96c33a
Sebastian Petersen <spetersen@microsoft.com>
2025-08-11 13:36:17 +0200

    Removing visualizer code.

--- Actions/AnalyzeDiagnosticLogs/AnalyzeDiagnosticLogs.ps1
+++ Actions/AnalyzeDiagnosticLogs/AnalyzeDiagnosticLogs.ps1
@@ -1,14 +1,7 @@
-Param(
-    [Parameter(HelpMessage = "Project to analyze", Mandatory = $false)]
-    [string] $project = '.'
-)
-
-$mdHelperPath = Join-Path -Path $PSScriptRoot -ChildPath "..\MarkDownHelper.psm1"
-Import-Module $mdHelperPath
-
 $errorLogsFolder = Join-Path $ENV:GITHUB_WORKSPACE "ErrorLogs"
 $errorLogFiles = Get-ChildItem -Path $errorLogsFolder -Filter "*.errorLog.json" -File -Recurse
 
+# Base SARIF structure
 $sarif = @{
     version = "2.1.0"
     '$schema' = "https://json.schemastore.org/sarif-2.1.0.json"
@@ -48,10 +41,12 @@ function GenerateSARIFJson {
         # Convert absolute path to relative path from repository root
         $absolutePath = $issue.locations[0].analysisTarget[0].uri
         $workspacePath = $ENV:GITHUB_WORKSPACE
-        Write-Host $workspacePath
-        $relativePath = $absolutePath.Replace('\', '/')
-        $relativePath = $relativePath.Replace($workspacePath, '').TrimStart('/')
-        $relativePath = $relativePath.Replace('D:/a/Al-Go_MultiProjectTest/Al-Go_MultiProjectTest/', '')
+        Write-Host "Workspace path: $workspacePath"
+        Write-Host "Absolute path: $absolutePath"
+        #$relativePath = $absolutePath.Replace('\', '/')
+        $relativePath = $absolutePath.Replace($workspacePath, '').TrimStart('/').Replace('\', '/')
+        Write-Host "Relative path: $relativePath"
+        #$relativePath = $relativePath.Replace('D:/a/Al-Go_MultiProjectTest/Al-Go_MultiProjectTest/', '')
 
         # Add result
         $sarif.runs[0].results += @{
@@ -68,8 +63,6 @@ function GenerateSARIFJson {
     }
 }
 
-$logHeaders = @('App', 'Warnings', 'Errors')
-$logRows = [System.Collections.ArrayList]@()
 $errorLogFiles | ForEach-Object {
     OutputDebug -message "Found error log file: $($_.FullName)"
     $fileName = $_.Name
@@ -77,27 +70,6 @@ $errorLogFiles | ForEach-Object {
     try {
         $errorLogContent = Get-Content -Path $_.FullName -Raw | ConvertFrom-Json
         GenerateSARIFJson -errorLogContent $errorLogContent
-        $numWarnings = 0
-        $numErrors = 0
-        if ($errorLogContent -and $errorLogContent.issues) {
-            $errorLogContent.issues | ForEach-Object {
-                if ($_.properties -and $_.properties.severity) {
-                    switch ($_.properties.severity) {
-                        'Warning' { $numWarnings++ }
-                        'Error' { $numErrors++ }
-                        default { OutputDebug -message "Unknown severity: $($_.properties.severity)" }
-                    }
-                }
-            }
-        }
-        else {
-            Write-Host "No issues found in error log file: $($_.FullName)"
-            OutputDebug -message "No issues found in error log file: $($_.FullName)"
-            Write-Host ($errorLogContent | ConvertTo-Json -Depth 10)
-        }
-        $appName = ($_.Name).Replace('.errorLog.json', '')
-        $logRow = @($appName, $numWarnings, $numErrors)
-        $logRows.Add($logRow) | Out-Null
     }
     catch {
         Write-Host "Failed to process $fileName"
@@ -105,16 +77,6 @@ $errorLogFiles | ForEach-Object {
     }
 }
 
-try {
-    Write-Host ($logRows | ConvertTo-Json -Depth 10)
-    $logTable = Build-MarkdownTable -Headers $logHeaders -Rows $logRows
-    Add-Content -Encoding UTF8 -path $ENV:GITHUB_STEP_SUMMARY -value "$($logTable.Replace("\n","`n"))"
-} catch {
-    Write-Host "Failed to build markdown table for error logs"
-    OutputDebug -message "Failed to build markdown table: $_"
-}
-
-
 $sarifJson = $sarif | ConvertTo-Json -Depth 10 -Compress
 Write-Host ($sarifJson)
-Set-Content -Path "output.sarif.json" -Value $sarifJson
+Set-Content -Path "$errorLogsFolder/output.sarif.json" -Value $sarifJson
